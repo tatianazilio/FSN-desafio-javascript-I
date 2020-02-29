b0ed7cc2f9eed8c6cb360b0c99e0a37393ee90fe
Tatiana Z T Moreira <tatianaztmoreira@MacBook-Pro-de-Tatiana.local>
2020-02-29 18:56:42 -0300

    criando as primeiras funcionalidades

--- index.js
+++ index.js
@@ -1,5 +1,122 @@
 // Base a ser utilizada
-const alunosDaEscola=[{nome:"Henrique",notas:[],cursos:[],faltas:5},{nome:"Edson",notas:[],cursos:[],faltas:2},{nome:"Bruno",notas:[10,9.8,9.6],cursos:[],faltas:0},{nome:"Guilherme",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"Full Stack",dataMatricula:new Date}],faltas:0},{nome:"Carlos",notas:[],cursos:[],faltas:0},{nome:"Lucca",notas:[10,9.8,9.6],cursos:[{nomeDoCurso:"UX",dataMatricula:new Date}],faltas:0}];
+const alunosDaEscola=[{
+        nome:"Henrique",
+        notas:[],
+        cursos:[],
+        faltas:5
+    },
+
+    {
+        nome:"Edson",
+        notas:[],
+        cursos:[],
+        faltas:2
+    },
+
+    {
+        nome:"Bruno",
+        notas:[10,9.8,9.6],
+        cursos:[],
+        faltas:0
+    },
+
+    {
+        nome:"Guilherme",
+        notas:[10,9.8,9.6],
+        cursos:[{nomeDoCurso:"Full Stack",dataMatricula:new Date}],
+        faltas:0
+    },
+    
+    {
+        nome:"Carlos",
+        notas:[],
+        cursos:[],
+        faltas:0
+    },
+
+    {
+        nome:"Lucca",
+        notas:[10,9.8,9.6],
+        cursos:[{nomeDoCurso:"UX",dataMatricula:new Date}],
+        faltas:0
+    }
+];
 
 
 // implementação
+function adicionarAluno(nome){
+ 
+    /*Essa função irá receber uma *string* que é nome do aluno a ser criado. 
+    E seguindo o modelo de aluno, o mesmo deverá ser inserido na lista de alunos.
+    A função deve devolver um feedback de sucesso, caso o aluno seja inserido corretamente.*/
+
+        alunosDaEscola.push({nome: nome,notas:[],cursos:[],faltas:0});
+        console.log(`O aluno ${nome} foi inserido com sucesso`);
+}
+   
+function listarAlunos(){
+    /*Com essa função o usuário poderá ver todos os alunos cadastrados atualmente no sistema. 
+    Vale dizer que As informações deverão ser exibidas em um formato amigável.*/
+        
+        console.log("=== LISTA DE ALUNOS ===");
+        console.log();
+
+        for(let i = 0; i < alunosDaEscola.length; i++) {
+            console.log("------------------");
+            console.log("Nome do aluno: ", alunosDaEscola[i].nome);
+            console.log("Notas: ", alunosDaEscola[i].notas);
+            let cursos = alunosDaEscola[i].cursos;
+            console.log("Cursos: ");
+            for(let i = 0; i < cursos.length; i++) {
+                console.log("    " + cursos[i].nomeDoCurso + " | Data da matrícula: " + cursos[i].dataMatricula);
+            }
+            console.log("Faltas: ", alunosDaEscola[i].faltas);
+        }
+}
+
+listarAlunos();
+
+function buscarAluno(nome){
+    /* Por meio dessa função, podemos pesquisar um aluno por nome na lista de aluno. Ela deverá exibir um feedback, tanto para quando encontrar o aluno, tanto quando não encontrar. E deverá devolver um aluno em seu retorno. */
+    
+
+    for (let i = 0; i < alunosDaEscola.length; i++) {
+        var nomeDoAluno = alunosDaEscola[i].nome;
+        var indiceDoAluno = nomeDoAluno.indexOf(nome);
+    }
+    
+    console.log(nomeDoAluno, indiceDoAluno);
+
+    if (indiceDoAluno >= 0) {
+        console.log(`O aluno ${nome} está matriculado na escola`);
+    } else {
+        console.log(`O aluno ${nome} não está matriculado na escola`);
+    }
+    return alunosDaEscola[indiceDoAluno];
+}
+
+buscarAluno("Bruno");
+
+    function matricularAluno(aluno/*:object*/, curso /*:string*/){
+    /* Essa funcionalidade irá permitir, cadastrar um aluno em um curso. 
+    Essa função só poderá ser executada em um aluno já devidamente cadastrado no sistema, e deverá armazenar a data atual no momento da matricula
+    Lembre-se de exibir o feedback para o usuário. */
+   }
+    function aplicarFalta(aluno/*:object*/){
+    /*
+     Ao receber um aluno devidamente cadastrado em nossa lista. Você deverá incrementar uma falta ao aluno. Você deverá dar um feedback ao concluir a tarefa. Só poderá aplicar falta em aluno se o mesmo tiver matriculado em um curso.
+    */
+    }
+    
+    function aplicarNota(aluno/*:object*/){
+    /*
+     Ao receber um aluno devidamente cadastrado em nossa lista. Você deverá adicionar uma nota ao aluno na sua lista de notas. Você deverá dar um feedback ao concluir a tarefa. Só poderá aplicar nota em aluno se o mesmo tiver matriculado em um curso.
+    */
+   }
+   
+     function aprovarAluno(aluno/*:object*/){
+     /* 
+     Ao receber um aluno devidamente cadastrado em nossa lista, deverá dizer se o mesmo está aprovado ou não. Os critérios de aprovação são: ter no máximo 3 faltas e média 7 em notas.
+     Só o aluno só poderá ser aprovado se o mesmo tiver matriculado em um curso.
+     */
+     }
